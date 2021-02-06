869846143851f8bd5f05c3d8f4b6f0289d17f481
George Williams <gmarvwill413@gmail.com>
2021-02-05 20:37:55 -0600

    Added sounds

--- MixedMessages.js
+++ MixedMessages.js
@@ -1,22 +1,12 @@
-let message1 = [
-  'doggie',
-  'cat',
-  'snake',
-  'bird',
-  'lion',
-  'chicken',
-  'tiger',
-  'bear',
-  'children',
-];
-
-const randomMessage = (array) => {
-  let arr = [];
-  for (let i = 0; i < array.length; i++) {
-    arr.push(array[Math.floor(Math.random() * array.length)]);
+var addTwoNumbers = function (l1, l2) {
+  const arr = [];
+  const rem = 0;
+  const add = 0;
+  for (let i = l1.length - 1; i > 0; i--) {
+    const number = l1[i] + l2[i];
+    if (number < 9) {
+      arr[i].push(number);
+    }
   }
   return arr;
 };
-
-let message = randomMessage(message1);
-console.log(message.join(' '));
