d3cc1aad24f8b86a1ca66d545aec51237e4b3260
George Williams <gmarvwill413@gmail.com>
2021-04-13 04:33:09 -0500

    Homepage images finished

---
+++ crown-clothing/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomepageComponents from './page-components/homepage.components.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HomepageComponents />
+    </div>
+  );
+}
+
+export default App;
--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     this.state = {
       monsters: [],
       searchField: '',
+      title: '',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -19,16 +20,17 @@ class App extends Component {
   }
 
   handleChange(e) {
-    this.setState({ searchField: e.target.value });
+    this.setState({ searchField: e.target.value, title: e.target.value });
   }
 
   render() {
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField, title } = this.state;
     const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase()),
     );
     return (
       <div className="App">
+        <h1>{title}</h1>
         <Searchbar
           placeholder="Search For A Monster"
           handleChange={this.handleChange}
