7ab362bfce19a06a4aba5c981bcb0c1c171ff31a
gmwill512 <gmarvwill413@gmail.com>
2021-05-27 09:23:22 -0500

    Insta Clone Finished

---
+++ instagram-clone/src/components/story-bar/story-bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './story-bar.css';
+
+function StoryBar() {
+  return (
+    <div className="storyBarContainer">
+      <div className="stories">
+        <h3> StoryBar</h3>
+      </div>
+    </div>
+  );
+}
+
+export default StoryBar;
